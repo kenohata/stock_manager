4acde13509432e0fbd90d42fb420d4ced2e243c0
Ken Ohata <ohata@daraf.co.jp>
2015-01-06 12:28:18 +0900

    add root

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :purchase_orders, only: [:create]
   resources :stock_orders, only: [:create]
 
+  root to: 'application#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

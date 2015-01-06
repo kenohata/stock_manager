21a2f1dbb618ffdcab1318e2bff05d59a70f5776
Ken Ohata <ohata@daraf.co.jp>
2015-01-06 11:13:48 +0900

    rails g model purchase_order_item

---
+++ db/migrate/20150106021340_create_purchase_order_items.rb
@@ -0,0 +1,11 @@
+class CreatePurchaseOrderItems < ActiveRecord::Migration
+  def change
+    create_table :purchase_order_items do |t|
+      t.integer :purchase_order_id
+      t.integer :product_id
+      t.integer :quantity
+
+      t.timestamps null: false
+    end
+  end
+end

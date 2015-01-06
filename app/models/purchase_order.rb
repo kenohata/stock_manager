5a1a30270a3ef747375d9cc442b73ea3c49a33ac
Ken Ohata <ohata@daraf.co.jp>
2015-01-06 11:56:04 +0900

    fix model

--- app/models/purchase_order.rb
+++ app/models/purchase_order.rb
@@ -2,4 +2,9 @@ class PurchaseOrder < ActiveRecord::Base
   has_many :items, class_name: PurchaseOrderItem, dependent: :destroy
 
   validates :orderer_name, presence: true, length: { maximum: 100 }
+  validates :item_size, length: { minimum: 1 }
+
+  def item_size
+    item.size
+  end
 end

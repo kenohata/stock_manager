8e89f0cb4bb7c2941a1791712a48938de8e4019b
Ken Ohata <ohata@daraf.co.jp>
2015-01-13 15:40:41 +0900

    include AbstractItem

--- app/models/purchase_order_item.rb
+++ app/models/purchase_order_item.rb
@@ -1,4 +1,6 @@
 class PurchaseOrderItem < ActiveRecord::Base
+  include AbstractItem
+
   belongs_to :order, class_name: PurchaseOrder, foreign_key: :purchase_order_id
   belongs_to :product
 
--- app/models/stock_order_item.rb
+++ app/models/stock_order_item.rb
@@ -1,4 +1,6 @@
 class StockOrderItem < ActiveRecord::Base
+  include AbstractItem
+
   belongs_to :order, class_name: StockOrder, foreign_key: :stock_order_id
   belongs_to :product
 

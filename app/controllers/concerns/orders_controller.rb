6bd6154d93a54131a12607cd5786376a9c3acabe
Ken Ohata <ohata@daraf.co.jp>
2015-01-13 16:10:02 +0900

    update OrdersController

--- app/controllers/concerns/orders_controller.rb
+++ app/controllers/concerns/orders_controller.rb
@@ -1,26 +1,39 @@
 class OrdersController < ApplicationController
+  before_action :set_new_order
 
   def new
-    @order = order_model.new
   end
 
   def create
-    @order = order_model.new
+    set_params_order
+
+    respond_to do |format|
+      if @order.save
+        format.json { render json: @products }
+      else
+        format.json { render json: @order.errors.full_messages }
+      end
+    end
   end
 
   private
 
   def params_order
-    params.require(:order).permit(:orderer_name)
+    params.require(:order).permit(:orderer_name, items: [:product_id, :quantity])
   end
 
-  def params_items
-    params.require(:order).permit(items: [:product_id, :quantity])
+  def set_new_order
+    @order = order_model.new
   end
 
   def set_params_order
-    @order.assign_attributes(params_order)
-    params_items[:items].each{ |item| @order.items.build(item) }
+    @order.assign_attributes(orderer_name: params_order[:orderer_name])
+
+    params_order[:items].each do |item|
+      product  = Product.find(item[:product_id])
+      quantity = item[:quantity]
+
+      @order.items.build(product: product, quantity: quantity) if quantity and quantity.to_i  .nonzero?
     end
   end
 end

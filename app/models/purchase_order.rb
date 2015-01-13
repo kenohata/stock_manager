fe219add6e785560ae86c80487d68f12f81fd6fa
Ken Ohata <ohata@daraf.co.jp>
2015-01-13 17:08:14 +0900

    fix validation

--- app/models/purchase_order.rb
+++ app/models/purchase_order.rb
@@ -4,6 +4,6 @@ class PurchaseOrder < ActiveRecord::Base
   has_many :items, class_name: PurchaseOrderItem, dependent: :destroy
 
   validates :orderer_name, presence: true, length: { maximum: 100 }
-  validates :items_size, length: { minimum: 1 }
+  validates :items_size, numericality: { greater_than: 0 }
   validates :items_size_all_nonzero?, inclusion: { in: [true] }
 end

862609ff4cdaa7c277b7f0c357b9ef6edd58a25d
Ken Ohata <ohata@daraf.co.jp>
2015-01-13 15:41:21 +0900

    include AbstractOrder

--- app/models/stock_order.rb
+++ app/models/stock_order.rb
@@ -1,10 +1,8 @@
 class StockOrder < ActiveRecord::Base
+  include AbstractOrder
+
   has_many :items, class_name: StockOrderItem, dependent: :destroy
 
   validates :orderer_name, presence: true, length: { maximum: 100 }
   validates :item_size, length: { minimum: 1 }
-
-  def item_size
-    item.size
-  end
 end

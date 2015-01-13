afc479005421f8039baeee34c22a34496fdc47b1
Ken Ohata <ohata@daraf.co.jp>
2015-01-13 15:42:24 +0900

    fix typo

--- app/models/purchase_order.rb
+++ app/models/purchase_order.rb
@@ -4,5 +4,5 @@ class PurchaseOrder < ActiveRecord::Base
   has_many :items, class_name: PurchaseOrderItem, dependent: :destroy
 
   validates :orderer_name, presence: true, length: { maximum: 100 }
-  validates :item_size, length: { minimum: 1 }
+  validates :items_size, length: { minimum: 1 }
 end

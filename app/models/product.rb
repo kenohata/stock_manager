bbb6705d067a50fad4953ced0cdebbbfe405595b
Ken Ohata <ohata@daraf.co.jp>
2015-01-13 17:08:48 +0900

    define Product#stock_quantity

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,15 @@ class Product < ActiveRecord::Base
   has_many :purchase_order_items, dependent: :destroy
 
   mount_uploader :image, ImageUploader
+
+  def stock_quantity
+    if @stock_quantity
+      @stock_quantity
+    else
+      accum    = ->(sum, item){ sum + item.quantity }
+      incoming = stock_order_items.inject(0, &accum)
+      outgoing = purchase_order_items.inject(0, &accum)
+      @stock_quantity = incoming - outgoing
+    end
+  end
 end

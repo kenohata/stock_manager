72f5a326d8816549a2e6f4f0d204693cdc730474
Ken Ohata <ohata@daraf.co.jp>
2015-01-19 15:02:21 +0900

    extract methods

--- app/models/concerns/abstract_item.rb
+++ app/models/concerns/abstract_item.rb
@@ -2,6 +2,10 @@ module AbstractItem
   extend ActiveSupport::Concern
 
   included do
+    validates :order, presence: true
+    validates :product, presence: true
+    validates :quantity, presence: true, numericality: { greater_than: 0, less_than: 127 }
+
     after_initialize do |item|
       item.quantity ||= 0
     end
--- app/models/stock_order_item.rb
+++ app/models/stock_order_item.rb
@@ -3,8 +3,4 @@ class StockOrderItem < ActiveRecord::Base
 
   belongs_to :order, class_name: StockOrder, foreign_key: :stock_order_id
   belongs_to :product
-
-  validates :order, presence: true
-  validates :product, presence: true
-  validates :quantity, presence: true, numericality: { greater_than: 0, less_than: 127 }
 end

d4f23e59a9fe39c24edbba035433beb7d1913bd7
Ken Ohata <ohata@daraf.co.jp>
2015-01-22 00:48:24 +0900

    disable validation

--- app/models/stock_order.rb
+++ app/models/stock_order.rb
@@ -3,7 +3,7 @@ class StockOrder < ActiveRecord::Base
 
   has_many :items, class_name: StockOrderItem, dependent: :destroy
 
-  validates :orderer_name, presence: true, length: { maximum: 100 }
+  # validates :orderer_name, presence: true, length: { maximum: 100 }
   validates :items_size, numericality: { greater_than: 0 }
   validates :items_size_all_nonzero?, inclusion: { in: [true] }
 end
